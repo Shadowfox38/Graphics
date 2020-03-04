6f700f9796cbd2b667cc811e6628b5503b4b26fd
Akhil Jain <46007832+Shadowfox38@users.noreply.github.com>
2020-03-04 12:28:55 +0530

    Add files via upload

---
+++ script_graphic.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## validate input
+test -n "$1"|| { echo "check input again"; exit 1; }
+
+## set standard build flags and test if exists/source ./bldflags
+stdclfags="gfx.c -L/usr/include/mysql -lmysqlclient -I/usr/include/mysql -lX11 -lm"             # add any standard flags you use.
+test -r ./bldflags && bldflags="`<./bldflags`"
+
+## call g++
+g++ "$1" $stdclfags  $bldflags ${@:3}
+
+exit 0
